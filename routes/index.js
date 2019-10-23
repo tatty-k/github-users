734d63a8f4d4280b31132a1c463a007838dc45f7
Tatty <tattyklass@gmail.com>
2019-10-23 14:03:04 -0700

    post route to make get request from github API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var rootURL = "http://api.github.com/";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {userData: null});
 });
+
+router.post('/',function(req, res){
+  request(
+    rootURL + 'users/' + req.body.username + 
+    '?access_token=' + process.env.GITHUB_TOKEN,
+    function(err, response, body){
+      res.render('index', {userData: body});
+    }
+  )
+})
 
 module.exports = router;
